526f535fe0c18af4cb8ed8333188fccb1143f99c
Lindsey Miller <lindc.miller@gmail.com>
2015-12-23 13:49:51 -0500

    favicon, dynamic click to expand side navigation, alignment of header and footer, more rows in main content

---
+++ js/youtube.js
@@ -0,0 +1,8 @@
+function showHide() {
+  var navigationExpand = document.querySelector(".navigation__expand");
+  if(navigationExpand.style.display == "none") {
+    navigationExpand.style.display != "none";
+  } else {
+    navigationExpand.style.display = "block";
+  }
+};
