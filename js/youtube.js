bbd3c70f04860f6f4b3ad9ebd15a326caa0f009c
Lindsey Miller <lindc.miller@gmail.com>
2015-12-29 17:09:05 -0500

    add styled content to expanding restricted button, style links in side nav, add 6th preview to large screen, widen preview image, perfect subscription hover

--- js/youtube.js
+++ js/youtube.js
@@ -16,27 +16,46 @@ function showHide() {
 
 //make these reusable for both
 
+
 function showHideLanguages() {
   var langExpand = document.querySelector(".language__expand");
   var countryExpand = document.querySelector(".country__expand");
+  var restrictedExpand = document.querySelector(".restricted__expand");
 
   if(langExpand.style.display !== "none") {
     langExpand.style.display = "none";
   } else {
     langExpand.style.display = "block";
     countryExpand.style.display = "none";
+    restrictedExpand.style.display = "none";
   }
 };
 
 function showHideCountries() {
   var langExpand = document.querySelector(".language__expand");
   var countryExpand = document.querySelector(".country__expand");
+  var restrictedExpand = document.querySelector(".restricted__expand");
 
   if(countryExpand.style.display !== "none") {
     countryExpand.style.display = "none";
   } else {
     countryExpand.style.display = "block";
     langExpand.style.display = "none";
+    restrictedExpand.style.display = "none";
+  }
+};
+
+function showHideRestricted() {
+  var langExpand = document.querySelector(".language__expand");
+  var countryExpand = document.querySelector(".country__expand");
+  var restrictedExpand = document.querySelector(".restricted__expand");
+  
+  if(restrictedExpand.style.display !== "none") {
+    restrictedExpand.style.display = "none";
+  } else {
+    restrictedExpand.style.display = "block";
+    langExpand.style.display = "none";
+    countryExpand.style.display = "none";
   }
 };
 
@@ -50,14 +69,31 @@ window.onload = function()  {
   for(var index = 0; index < allExpandTabs.length; index++) {
     allExpandTabs[index].addEventListener( 'click' , setActiveFill );
   }
+
+  var allExpandButtons = document.getElementsByClassName("button__expand");
+  for(var index = 0; index < allExpandButtons.length; index++) {
+    allExpandButtons[index].addEventListener( 'click' , setActiveDarken );
+  }
 };
 
 function setActiveUnderline(event) {
-  document.querySelector(".active__underline").classList.remove('active__underline');
+  document.querySelector('.active__underline').classList.remove('active__underline');
   event.target.classList.add('active__underline');
 };
 
 function setActiveFill(event) {
-  document.querySelector(".active__fill").classList.remove('active__fill');
+  document.querySelector('.active__fill').classList.remove('active__fill' );
   event.target.classList.add('active__fill');
+
+  // document.querySelector(".active__fill-icon").classList.remove('active__fill-icon');
+  // event.target.classList.add('active__fill-icon');
+};
+
+function setActiveDarken(event) {
+  if (document.querySelector('.active__darken') == null) {
+    event.target.classList.add('active__darken');
+  } else {
+    document.querySelector('.active__darken').classList.remove('active__darken');
+    event.target.classList.add('active__darken');
+  }
 };
